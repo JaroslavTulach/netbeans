0fba964a8a26697cef9deca06d884aad18b78583
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2019-04-30 10:52:08 +0200

    Using LoopNode and RepeatingNode

--- profiler/profiler.oql/test/unit/src/org/netbeans/modules/profiler/oql/fast/OQLLanguage.java
+++ profiler/profiler.oql/test/unit/src/org/netbeans/modules/profiler/oql/fast/OQLLanguage.java
@@ -24,6 +24,9 @@ import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.frame.FrameSlot;
+import com.oracle.truffle.api.frame.FrameSlotTypeException;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.ArityException;
 import com.oracle.truffle.api.interop.InteropLibrary;
@@ -31,24 +34,24 @@ import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.interop.UnknownIdentifierException;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.interop.UnsupportedTypeException;
-import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
-import com.oracle.truffle.api.library.LibraryFactory;
+import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
+import com.oracle.truffle.api.nodes.LoopNode;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.nodes.RepeatingNode;
 import com.oracle.truffle.api.nodes.RootNode;
 import java.io.File;
 import java.net.URI;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.function.Consumer;
 import org.netbeans.lib.profiler.heap.Heap;
 import org.netbeans.lib.profiler.heap.HeapFactory;
 import org.netbeans.lib.profiler.heap.Instance;
 import org.netbeans.lib.profiler.heap.JavaClass;
 import org.netbeans.lib.profiler.heap.PrimitiveArrayInstance;
 import org.netbeans.modules.profiler.oql.engine.api.impl.TreeIterator;
-import org.openide.util.Exceptions;
 
 @TruffleLanguage.Registration(
     byteMimeTypes = "application/oql", name = "oql", id = "oql"
@@ -116,36 +119,85 @@ final class HeapObject implements TruffleObject {
             return args[0];
         }
 
-        @Specialization(limit = "3")
+        static DirectCallNode createLoop() {
+            return Truffle.getRuntime().createDirectCallNode(
+                Truffle.getRuntime().createCallTarget(new DispatchRoot())
+            );
+        }
+
+        static DirectCallNode getUncached() {
+            return null;
+        }
+
+        @Specialization
         static Object invokeMember(
             HeapObject heap, String member, Object[] arguments,
-            @Cached("member") String cachedMember, 
-            @CachedLibrary(value = "receiver(arguments)") InteropLibrary interop
+            @Cached("createLoop()") DirectCallNode loop,
+            @Cached("member") String cachedMember
         ) throws UnsupportedMessageException, ArityException, UnknownIdentifierException, UnsupportedTypeException {
             if ("forEachObject".equals(cachedMember)) {
-                Consumer<Object> consumer = (value) -> {
-                    try {
-                        InstanceObject obj = new InstanceObject((Instance)value);
-                        interop.execute(receiver(arguments), obj);
-                    } catch (UnsupportedTypeException | ArityException | UnsupportedMessageException ex) {
-                        CompilerDirectives.transferToInterpreter();
-                        throw ex.raise();
-                    }
-                };
                 Iterator it = getInstances(heap.heap, arguments);
-                Object[] buffer = new Object[1024];
-                while (it.hasNext()) {
-                    dispatchInstances(it, consumer, buffer);
-                }
+                loop.call(it, receiver(arguments));
                 return heap;
             }
             throw UnknownIdentifierException.create(member);
         }
 
-        private static void dispatchInstances(Iterator it, Consumer<Object> consumer, Object[] buffer) {
-            int len = fillInBuffer(it, buffer);
-            for (int i = 0; i < len; i++) {
-                consumer.accept(buffer[i]);
+        private static class DispatchRoot extends RootNode {
+            private static final FrameDescriptor DISPATCH_LOOP;
+            private static FrameSlot IT;
+            private static FrameSlot RECEIVER;
+            static {
+                DISPATCH_LOOP = new FrameDescriptor();
+                IT = DISPATCH_LOOP.addFrameSlot("it");
+                RECEIVER = DISPATCH_LOOP.addFrameSlot("receiver");
+            }
+            @Child
+            private LoopNode loop;
+            @Child
+            private DispatchInstances dispatch;
+
+            public DispatchRoot() {
+                super(null, DISPATCH_LOOP);
+                dispatch = new DispatchInstances();
+                loop = Truffle.getRuntime().createLoopNode(dispatch);
+            }
+
+            @Override
+            public Object execute(VirtualFrame frame) {
+                frame.setObject(IT, frame.getArguments()[0]);
+                frame.setObject(RECEIVER, frame.getArguments()[1]);
+                loop.executeLoop(frame);
+                return null;
+            }
+        }
+
+        private static class DispatchInstances extends Node implements RepeatingNode {
+            private final Object[] buffer = new Object[1024];
+            @Child
+            private InteropLibrary interop = InteropLibrary.getFactory().createDispatched(3);
+
+            @Override
+            public boolean executeRepeating(VirtualFrame frame) {
+                try {
+                    Iterator it = (Iterator) frame.getObject(DispatchRoot.IT);
+                    Object receiver = frame.getObject(DispatchRoot.RECEIVER);
+
+                    int len = fillInBuffer(it, buffer);
+                    for (int i = 0; i < len; i++) {
+                        InstanceObject obj = new InstanceObject((Instance) buffer[i]);
+                        try {
+                            interop.execute(receiver, obj);
+                        } catch (UnsupportedTypeException | ArityException | UnsupportedMessageException ex) {
+                            CompilerDirectives.transferToInterpreter();
+                            throw raise(RuntimeException.class, ex);
+                        }
+                    }
+                    return len == buffer.length;
+                } catch (FrameSlotTypeException ex) {
+                    CompilerDirectives.transferToInterpreter();
+                    throw raise(RuntimeException.class, ex);
+                }
             }
         }
 
@@ -183,6 +235,9 @@ final class HeapObject implements TruffleObject {
         }
     }
 
+    static <E extends Exception> E raise(Class<E> aClass, Throwable ex) throws E {
+        throw (E) ex;
+    }
 
 }
 
